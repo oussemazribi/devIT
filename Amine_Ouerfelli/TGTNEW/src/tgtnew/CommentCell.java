7ddab7b75a7f3c44e6d7bc4e3a8832701cdda40b
totoRina71 <mohamedamine.ouerfelli@esprit.tn>
2020-02-19 09:13:42 +0100

    amine

---
+++ Amine_Ouerfelli/TGTNEW/src/tgtnew/CommentCell.java
@@ -0,0 +1,174 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tgtnew;
+
+import Services.ServiceCommentaire;
+import entities.Commentaire;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.SQLException;
+import java.util.List;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import static tgtnew.HomeController.iduser_1;
+
+/**
+ *
+ * @author asus
+ */
+public class CommentCell extends ListCell<Commentaire>{
+    @FXML
+    private Label username;
+    @FXML
+    private Label commentbody;
+    @FXML
+    private Label date;
+    @FXML
+    private Button delete;
+    @FXML
+    private Button update;
+    @FXML
+    private ImageView userimage;
+    @FXML
+    private AnchorPane combody;
+    /************************************************/
+    String username1="amine";
+    private FXMLLoader Loader;
+    private TextField tf=new TextField();
+    private Button bt=new Button();
+    ServiceCommentaire serc= new ServiceCommentaire();
+    /************************************************/
+    @FXML
+    public void modifier()
+    {
+        tf.setLayoutY(61.0);
+        tf.setPrefHeight(101.0);
+        tf.setPrefWidth(595.0);
+        combody.getChildren().remove(commentbody);
+        combody.getChildren().add(tf);
+        combody.getChildren().remove(update);
+        combody.getChildren().remove(delete);
+        bt.setLayoutX(374.0);
+        bt.setLayoutY(162.0);
+        bt.setText("Completer!");
+        combody.getChildren().add(bt);
+    }
+    @FXML
+    public void supprimer() throws SQLException
+    {
+        serc.supprimer(this.getItem());
+        this.getListView().getItems().remove(this.getItem());
+ 
+       
+        
+        
+        
+        
+    }
+    
+       public void modifier1() throws SQLException
+    {   
+        this.getItem().setcontenu(tf.getText());
+         Commentaire c= this.getItem();
+         if(serc.modifier(this.getItem()))
+         {  
+        
+        combody.getChildren().remove(bt);
+        combody.getChildren().remove(tf);
+        combody.getChildren().add(commentbody);
+        combody.getChildren().add(update);
+        combody.getChildren().add(delete);
+        List l1=serc.readAll();
+        this.getListView().getItems().clear();
+        this.getListView().getItems().addAll(l1);
+        
+        
+         }
+             
+    }
+         EventHandler<ActionEvent> event = new EventHandler<ActionEvent>() { 
+            @Override
+            public void handle(ActionEvent e)
+            {
+                try {
+                    modifier1();
+                } catch (SQLException ex) {
+                    System.out.println(ex.getMessage());
+                }
+            }
+        }; 
+    
+    
+    private void loadFXML() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("commentaire.fxml"));
+            loader.setController(this);
+            loader.setRoot(this);
+            loader.load();
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    CommentCell() {
+        loadFXML();
+    }
+
+
+    @Override
+        protected void updateItem(Commentaire com, boolean empty) {
+                super.updateItem(com, empty);
+
+        if(empty || com== null) {
+
+            setText(null);
+            setGraphic(null);
+
+        } else {
+         
+            date.setText(com.getdate().toString());
+            commentbody.setText(com.getcontenu());
+         if(this.getItem().getiduser()!=iduser_1)
+         {
+             update.setVisible(false);
+             delete.setVisible(false);
+         }
+         else
+         {
+            update.setVisible(true);
+            delete.setVisible(true);
+         }
+            bt.setOnAction(event);
+                        InputStream input1 =getClass().getResourceAsStream("/image/supprimer.png");
+            InputStream input2 =getClass().getResourceAsStream("/image/modifier.png");
+ 
+        Image image = new Image(input1);
+        Image image1 = new Image(input2);
+        
+        ImageView imageView1 = new ImageView(image);
+        ImageView imageView2 = new ImageView(image1);
+        delete.setGraphic(imageView1);
+        update.setGraphic(imageView2) ;
+         InputStream im =getClass().getResourceAsStream("/image/download.png");
+          Image imageuser1 = new Image(im);
+        userimage.setImage(imageuser1);
+        username.setText(username1);
+        
+            setGraphic(combody);
+        }
+        
+    }
+}
---
+++ Amine_Ouerfelli/TGTNEW/src/tgtnew/HomeController.java
@@ -0,0 +1,128 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tgtnew;
+
+import Services.ServicePCVR;
+import Services.ServicePublication;
+import entities.Commentaire;
+import entities.Publication;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author asus
+ */
+public class HomeController implements Initializable {
+    static int iduser_1=1;
+    static String username="amine";
+    
+    @FXML
+    private Button ajout;
+    @FXML
+    private ListView<Publication> listepublication;
+    
+   /**********************************/
+    ServicePublication serp=new ServicePublication();
+    ServicePCVR serPCVR= new ServicePCVR();
+     List<Publication> l1;
+     List<Commentaire> l2;
+    PublicationCellFactory cp;
+    @FXML
+    private Button actualiser;
+    @FXML
+    private Button stat;
+    public HomeController() {
+        this.cp = new PublicationCellFactory();
+        this.listepublication = new ListView<Publication>();
+    }
+     
+     /*********************************/
+    InputStream im =getClass().getResourceAsStream("/image/download.png");
+    Image imageuser1 = new Image(im);
+    @FXML
+ 
+    ImageView imageuser ;
+    
+    
+    /**
+     *
+     * @param url
+     * @param rb
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+           try {
+            l1=serp.readAll();
+           } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+           }
+          listepublication.setCellFactory(cp);
+          listepublication.getItems().addAll(l1);
+           ajout.setStyle("-fx-background-color: darkslateblue; -fx-text-fill: white;-fx-background-radius: 10px;"); 
+           actualiser.setStyle("-fx-background-color: darkslateblue; -fx-text-fill: white;-fx-background-radius: 10px;");
+            InputStream input1 =getClass().getResourceAsStream("/image/ajouter-100-.png");
+            InputStream input2 =getClass().getResourceAsStream("/image/actualiser-90-.png");
+            
+            
+ 
+        Image image = new Image(input1);
+        Image image1 = new Image(input2);
+        
+        ImageView imageView1 = new ImageView(image);
+        ImageView imageView2 = new ImageView(image1);
+        imageuser.setImage(imageuser1);
+        ajout.setGraphic(imageView1);
+        actualiser.setGraphic(imageView2);
+    }     
+   
+
+    @FXML
+    private void ajouter(ActionEvent event) throws IOException
+    {
+     Parent root = FXMLLoader.load(getClass().getResource("AjoutPub.fxml"));
+     Scene scene = new Scene(root);
+     Stage stage=new Stage();
+     stage.setScene(scene);
+     stage.show();
+    }
+
+    @FXML
+    private void actualiser(ActionEvent event) {
+            try {
+            l1=serp.readAll();
+           } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+           }
+        listepublication.getItems().clear();
+        listepublication.getItems().addAll(l1);
+    }
+
+    @FXML
+    private void stat(ActionEvent event) throws IOException {
+     Parent root = FXMLLoader.load(getClass().getResource("postsStatics.fxml"));
+     Scene scene = new Scene(root);
+     Stage stage=new Stage();
+     stage.setScene(scene);
+     stage.show();
+    }
+    
+}
