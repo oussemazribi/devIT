e8c6599bc8d064d976f6185a69fbbdf968484447
oussema <oussema.zribi@esprit.tn>
2020-02-23 02:10:09 +0100

    22/02/2020

--- oussema_zribi/tunisianGot/src/tunisiangot/AffichageAnnonceController.java
+++ oussema_zribi/tunisianGot/src/tunisiangot/AffichageAnnonceController.java
@@ -5,29 +5,47 @@
  */
 package tunisiangot;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
-import java.util.Optional;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.beans.binding.Bindings;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
-import javafx.scene.control.Alert;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.ToolBar;
-import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.Label;
+import javafx.scene.control.Separator;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.TouchEvent;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+import static org.opencv.core.Core.circle;
+
 import tungottalent.Entite.Annonce;
 import tungottalent.Service.ServiceAnnonce;
 import tungottalent.Service.ServiceUser;
@@ -39,27 +57,20 @@ import tungottalent.Service.ServiceUser;
  */
 public class AffichageAnnonceController implements Initializable {
 
+    Stage window;
+
     @FXML
-    private TableColumn<Annonce, String> ColumnDescription;
-    @FXML
-    private TableColumn<Annonce, String> ColumnNom;
-    @FXML
-    private TableColumn<Annonce, String> ColumnPrix;
-    @FXML
-    private TableColumn<Annonce, String> ColumnProduit;
-    @FXML
-    private TableView<Annonce> tableAnnonce;
+    private FlowPane flowPaneAnnonce;
 
     @FXML
     private Button plus;
-    @FXML
-    private Button Pub;
-    
-    private Button btnQuitter;
-    
-    ImageView quitter;
+
+
+    private final Background focusBackground = new Background(new BackgroundFill(Color.GREY, CornerRadii.EMPTY, Insets.EMPTY));
+    private final Background unfocusBackground = new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY));
+
     /**
-     * 
+     *
      * Initializes the controller class.
      */
     @Override
@@ -73,64 +84,168 @@ public class AffichageAnnonceController implements Initializable {
     ServiceAnnonce ser = new ServiceAnnonce();
     ServiceUser ser1 = new ServiceUser();
 
-    @FXML
     void AfficherAnnonce() throws SQLException {
-        tableAnnonce.setEditable(true);
 
+        
         ObservableList<Annonce> list = FXCollections.observableArrayList(ser.readAll());
-        // System.out.println(list);
-        ColumnDescription.setCellValueFactory(new PropertyValueFactory<Annonce, String>("Description"));
-        ColumnNom.setCellValueFactory(new PropertyValueFactory<Annonce, String>("Nom"));
-        ColumnProduit.setCellValueFactory(new PropertyValueFactory<Annonce, String>("user"));
-        ColumnPrix.setCellValueFactory(new PropertyValueFactory<Annonce, String>("Prix"));
-        tableAnnonce.setItems(list);
 
+        //System.out.println("We're right here for now ");
+        ArrayList<Separator> as = new ArrayList<>();
+        ArrayList<VBox> vbx = new ArrayList<>();
+        ArrayList<HBox> btnP = new ArrayList<>();
+
+        for (int i = 0; i < list.size(); i++) {
+
+            Separator h = new Separator(Orientation.VERTICAL);
+            h.setPrefWidth(17);
+            h.setPrefHeight(24);
+            h.setVisible(false);
+            as.add(h);
+
+            VBox VBoxAnnonce = new VBox();
+
+            VBoxAnnonce.setSpacing(7);
+            VBoxAnnonce.setStyle("-fx-background-color : #FFFFFF;");
+            //VBoxComp.setStyle("-fx-border-color : #2B48E8;");
+            VBoxAnnonce.setAlignment(Pos.CENTER);
+            VBoxAnnonce.setPrefHeight(265);
+            VBoxAnnonce.setPrefWidth(230);
+            HBox panne = new HBox();
+            panne.setSpacing(5);
+            panne.setStyle("-fx-background-color : #FFFFFF;");
+            //VBoxComp.setStyle("-fx-border-color : #2B48E8;");
+            panne.setAlignment(Pos.CENTER);
+            panne.setPrefHeight(30);
+            panne.setPrefWidth(80);
+
+            Circle c = new Circle(100);
+
+//                ImageView img = new ImageView(new Image(new FileInputStream("C:\\Users\\loume78\\Desktop\\Mon_cv\\loume.png")));
+//                img.setFitHeight(10);
+//                img.setFitWidth(20);
+            try {
+                c.setFill(new ImagePattern(new Image(new FileInputStream("C:/wamp64/www/Images_PI/" + list.get(i).getImages()))));
+            } catch (FileNotFoundException ex) {
+                System.out.println(ex.getMessage());
+            }
+
+            VBoxAnnonce.getChildren().add(c);
+
+            Label titreComp = new Label("Nom : " + list.get(i).getNom());
+            Label description = new Label("Description: " + list.get(i).getDescription());
+            //Label mail = new Label("Mail: " + listUs.get(i).getMail());
+            //Label tel = new Label("téléphone: " + listUs.get(i).getNumTel());
+            //Label etat = new Label();
+            //Label adresse = new Label("Adresse: " + listUs.get(i).getAdresse().getVille() + "," + listUs.get(i).getAdresse().getPays());
+
+            int idAnnonce = list.get(i).getIdAnnonce();
+            Annonce a1 = ser.findById(idAnnonce);
+
+//            VBoxAnnonce.getOnMouseClicked(new EventHandler<MouseEvent>() {
+//                @Override
+//                public void handle(MouseEvent event) {
+//                    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+//                }
+//            });
+            VBoxAnnonce.backgroundProperty().bind(Bindings
+                    .when(VBoxAnnonce.focusedProperty())
+                    .then(focusBackground)
+                    .otherwise(unfocusBackground)
+            );
+            VBoxAnnonce.setOnMouseClicked((e)
+                    -> {
+                VBoxAnnonce.requestFocus();
+
+                try {
+                   // Stage window = new Stage();
+                 
+                   // Parent root2;
+              FXMLLoader LOADER = new FXMLLoader(getClass().getResource("DetailAnnonce.fxml"));
+                    Parent root2 = LOADER.load();
+                   // Scene scene = new Scene(root2);
+                    VBoxAnnonce.getScene().setRoot(root2);
+                   // window.show();
+
+                } catch (IOException ex) {
+                    Logger.getLogger(AffichageAnnonceController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
+            });
+
+            //   panne.getChildren().add(btnDetail);
+            VBoxAnnonce.getChildren().add(panne);
+            VBoxAnnonce.getChildren().add(titreComp);
+            VBoxAnnonce.getChildren().add(description);
+
+            vbx.add(VBoxAnnonce);
+            btnP.add(panne);
+            flowPaneAnnonce.getChildren().add(vbx.get(i));
+
+            flowPaneAnnonce.getChildren().add(as.get(i));
+
+            if (i == 0) {
+                System.out.println("i=0 llllll");
+            } else if (((i + 1) % 2) == 0) {
+                Separator sepHoriz = new Separator(Orientation.HORIZONTAL);
+                sepHoriz.setPrefWidth(364);
+                sepHoriz.setPrefHeight(30);
+                sepHoriz.setVisible(false);
+                flowPaneAnnonce.getChildren().add(sepHoriz);
+
+            }
+        }
     }
 
+//    @FXML
+//    void detail(ActionEvent event) throws IOException {
+//
+//        Parent root2 = FXMLLoader.load(getClass().getResource("DetailAnnonce.fxml"));
+//        Scene scene = new Scene(root2);
+//
+//    }
     @FXML
     void ajout(ActionEvent event) throws IOException {
 
         FXMLLoader LOADER = new FXMLLoader(getClass().getResource("AjoutAnnonce.fxml"));
         Parent root1 = LOADER.load();
-        
+
         plus.getScene().setRoot(root1);
 
     }
-
-    @FXML
-    void Pub(ActionEvent event) throws IOException {
-
-        FXMLLoader LOADER = new FXMLLoader(getClass().getResource("Ajoutpublicite.fxml"));
-        Parent root2 = LOADER.load();
-        Pub.getScene().setRoot(root2);
-
-    }
-
-    public void btnSupprimerAction(ActionEvent event) throws IOException, SQLException {
-        ServiceAnnonce ser = new ServiceAnnonce();
-        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-        
-//        quitter = new ImageView(new Image(getClass().getResourceAsStream("/add.png")));
-//        btnQuitter.setGraphic(quitter);
-
-        alert.setTitle("Suppression ");
-        alert.setContentText("Voulez-vous vraiment supprimer cette Competition ?");
-        Optional<ButtonType> result = alert.showAndWait();
-        if (result.get() == ButtonType.OK) {
-            ser.delete(tableAnnonce.getSelectionModel().getSelectedItem());
-            this.AfficherAnnonce();
-        } else {
-            this.AfficherAnnonce();
-
-        }
-    }
-    
-       public void btnModifierAction(ActionEvent event) {
-           
-           
-           
-           
-       } 
-    
+//
+//    @FXML
+//    void Pub(ActionEvent event) throws IOException {
+//
+//        FXMLLoader LOADER = new FXMLLoader(getClass().getResource("Ajoutpublicite.fxml"));
+//        Parent root2 = LOADER.load();
+//        Pub.getScene().setRoot(root2);
+//
+//    }
+//
+//    public void btnSupprimerAction(ActionEvent event) throws IOException, SQLException {
+//        ServiceAnnonce ser = new ServiceAnnonce();
+//        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+//        
+////        quitter = new ImageView(new Image(getClass().getResourceAsStream("/add.png")));
+////        btnQuitter.setGraphic(quitter);
+//
+//        alert.setTitle("Suppression ");
+//        alert.setContentText("Voulez-vous vraiment supprimer cette Competition ?");
+//        Optional<ButtonType> result = alert.showAndWait();
+//        if (result.get() == ButtonType.OK) {
+//            ser.delete(tableAnnonce.getSelectionModel().getSelectedItem());
+//            this.AfficherAnnonce();
+//        } else {
+//            this.AfficherAnnonce();
+//
+//        }
+//    }
+//    
+//       public void btnModifierAction(ActionEvent event) {
+//           
+//           
+//           
+//           
+//       } 
 
 }
