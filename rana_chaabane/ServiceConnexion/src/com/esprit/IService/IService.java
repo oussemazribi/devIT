e008254d686ba34dc5ee92bd4f80e0cb870b94a6
ranachaabane <49126278+ranachaabane@users.noreply.github.com>
2020-02-24 11:28:16 +0100

    24/02/2020

---
+++ rana_chaabane/ServiceConnexion/src/com/esprit/IService/IService.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.esprit.IService;
+
+import com.esprit.Entite.Message;
+import com.esprit.Entite.User;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.List;
+
+/**
+ *
+ * @author House
+ * @param <T>
+ */
+public interface IService<T> {
+
+    void ajouter(T t) throws SQLException;
+
+    boolean delete(int id_Message) throws SQLException;
+
+    boolean transfer(int id_Message, int id_Conversation) throws SQLException;
+
+    public boolean SeenMessage(int id_Message) throws SQLException;
+
+    List<T> readAll() throws SQLException;
+
+    /**
+     *
+     * @param u
+     * @return
+     * @throws SQLException
+     */
+    public List<Message> messageenvoyees(User u) throws SQLException;
+    // public List<Message> Messagerecu(User u )  throws SQLException;
+
+    public long countNbUnreadConversations(int iduser) throws SQLException;
+
+    public List<Message> recherche(String recherche) throws SQLException;
+}
---
+++ rana_chaabane/ServiceConnexion/src/com/pidev/test/Test.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+*/
+package com.pidev.test;
+
+import com.pidev.Entite.Conversation;
+import com.pidev.Entite.Message;
+import com.pidev.Entite.User;
+import com.pidev.Service.ServiceConversation;
+import com.pidev.Service.ServiceMessage;
+import com.pidev.Service.ServiceUser;
+import com.pidev.Utils.DataBase;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+/**
+ *
+ * @author House
+*/
+public class Test {
+
+    public static void main(String[] args) {
+
+        ServiceMessage ser = new ServiceMessage();
+        ServiceConversation sc = new ServiceConversation() ;
+        ServiceUser su = new ServiceUser();
+        Message m1 = new Message(1, 2, "aaslemaaa", 9) ;
+        User u1 = new User();
+         User userme = new User(1);
+        try {
+            //  su.findbyimage(1);
+            // System.err.println("hello");
+          
+            List<Message> MapM = ser.messageenvoyees(userme,34); 
+            System.out.println(MapM);
+        } catch (SQLException ex) {
+            Logger.getLogger(Test.class.getName()).log(Level.SEVERE, null, ex);
+        }
+   
+          //  sc.readorder(2);
+           // sc.Nom(2);
+            //sc.image(2);
+            
+            // ser.ajouter(m1);
+          
+            //sc.ajouter(c);
+            
+            //  sc.deleteAd(3) ;
+            // sc.update("rana", 0, 0, 4);
+            //ser.transfer(18,  5);
+            //  ser.SeenMessage(44) ;
+            // ser.countNbUnreadConversations(2) ;
+            // ser.readAllforuser(2);
+            //sc.tester(3, 2) ;
+      
+       
+       
+    }
+
+        }
+ 
